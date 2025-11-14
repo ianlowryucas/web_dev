f0d48593f02bcbe6dfc60433fbb05cbd365521dd
ianlowryucas <ian.lowry@ucas-edu.net>
2025-11-14 13:41:52 -0700

    title fix

--- name_generator.py
+++ name_generator.py
@@ -1,6 +1,36 @@
 import random
 i=0
+letter_first=0
+letter_second=0
+letter_third=0
+letter_fourth=0
+letter_fifth=0
 def letter():
     while i<5:
         rand=random.randint(1, 26)
-        
+        if rand==1:
+            letter_first='a'
+        elif rand==2:
+            letter_first='b'
+        elif rand==3:
+            letter_first='c'
+        elif rand==3:
+            letter_first='d'
+        elif rand==3:
+            letter_first='e'
+        elif rand==3:
+            letter_first='f'
+        elif rand==3:
+            letter_first='g'
+        elif rand==3:
+            letter_first='h'
+        elif rand==3:
+            letter_first='i'
+        elif rand==3:
+            letter_first='j'
+        elif rand==3:
+            letter_first='c'
+        elif rand==3:
+            letter_first='c'
+        elif rand==3:
+            letter_first='c'
