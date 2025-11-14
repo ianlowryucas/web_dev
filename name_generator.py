b7f34c1f1df2ac6698ac88923a1e0ec5fff5b7a0
pythonprogram <ian.lowry@ucas-edu.net>
2025-11-14 14:41:59 -0700

    Update name_generator.py

--- name_generator.py
+++ name_generator.py
@@ -14,48 +14,101 @@ def letter():
             letter_first='b'
         elif rand==3:
             letter_first='c'
-        elif rand==3:
+        elif rand==4:
             letter_first='d'
-        elif rand==3:
+        elif rand==5:
             letter_first='e'
-        elif rand==3:
+        elif rand==6:
             letter_first='f'
-        elif rand==3:
+        elif rand==7:
             letter_first='g'
-        elif rand==3:
+        elif rand==8:
             letter_first='h'
-        elif rand==3:
+        elif rand==9:
             letter_first='i'
-        elif rand==3:
+        elif rand==10:
             letter_first='j'
-        elif rand==3:
+        elif rand==11:
             letter_first='k'
-        elif rand==3:
+        elif rand==12:
             letter_first='l'
-        elif rand==3:
+        elif rand==13:
             letter_first='m'
-        elif rand==2:
+        elif rand==14:
             letter_first='n'
-        elif rand==3:
+        elif rand==15:
             letter_first='o'
-        elif rand==3:
+        elif rand==16:
             letter_first='p'
-        elif rand==3:
+        elif rand==17:
             letter_first='q'
-        elif rand==3:
+        elif rand==18:
             letter_first='r'
-        elif rand==3:
+        elif rand==19:
             letter_first='s'
-        elif rand==3:
+        elif rand==20:
             letter_first='t'
-        elif rand==3:
+        elif rand==21:
             letter_first='u'
-        elif rand==3:
+        elif rand==22:
             letter_first='v'
-        elif rand==3:
+        elif rand==23:
             letter_first='w'
-        elif rand==3:
+        elif rand==24:
             letter_first='x'
-        elif rand==3:
+        elif rand==25:
             letter_first='y'
-
+        else:
+            letter_first='z'
+        if rand==1:
+            letter_second='a'
+        elif rand==2:
+            letter_second='b'
+        elif rand==3:
+            letter_second='c'
+        elif rand==4:
+            letter_second='d'
+        elif rand==5:
+            letter_second='e'
+        elif rand==6:
+            letter_second='f'
+        elif rand==7:
+            letter_second='g'
+        elif rand==8:
+            letter_second='h'
+        elif rand==9:
+            letter_second='i'
+        elif rand==10:
+            letter_second='j'
+        elif rand==11:
+            letter_second='k'
+        elif rand==12:
+            letter_second='l'
+        elif rand==13:
+            letter_second='m'
+        elif rand==14:
+            letter_second='n'
+        elif rand==15:
+            letter_first='o'
+        elif rand==16:
+            letter_first='p'
+        elif rand==17:
+            letter_first='q'
+        elif rand==18:
+            letter_first='r'
+        elif rand==19:
+            letter_first='s'
+        elif rand==20:
+            letter_first='t'
+        elif rand==21:
+            letter_first='u'
+        elif rand==22:
+            letter_first='v'
+        elif rand==23:
+            letter_first='w'
+        elif rand==24:
+            letter_first='x'
+        elif rand==25:
+            letter_first='y'
+        elif rand==26:
+            letter_first='z'
