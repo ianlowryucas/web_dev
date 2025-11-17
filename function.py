fae05ed3ec4df39458625247b537d765e6e8c308
ianlowryucas <ian.lowry@ucas-edu.net>
2025-11-17 13:43:44 -0700

    new

---
+++ function.py
@@ -0,0 +1,6 @@
+hi=0
+def iffy():
+    if hi<100:
+        hi+=1
+        print(hi)
+iffy()
---
+++ hampster.py
@@ -0,0 +1,7 @@
+i=0
+def average_child():
+    while i<1:
+        print('nuh uh')
+        print('yuh huh')
+#average_child()
+    
