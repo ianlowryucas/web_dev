a9210c4af64baedf6ba8f4c1949d2020f6e491ba
ianlowryucas <ian.lowry@ucas-edu.net>
2025-11-24 13:44:40 -0700

    magic

---
+++ python.py
@@ -0,0 +1,7 @@
+days=365
+def wow():
+    while days>0:
+        print("mario will steal your liver in", days, "days")
+        days-=1
+wow()
+
---
+++ ur_ma.py
@@ -0,0 +1,5 @@
+a=input("explode santi yes/no : ")
+if a=='yes':
+    print('good')
+else:
+    input('why',a)
